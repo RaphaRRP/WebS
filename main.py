dfa6f6c456a2ae08d9cc35477d17ef36de64a1ab
não programo em java <126816247+RaphaRRP@users.noreply.github.com>
2023-06-14 16:22:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from PyQt5 import uic, QtGui
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QPushButton
+
+
+class Telas():
+    def __init__(self):
+    app = QtWidgets.QApplication([])
+    
+    app.exec()
+
+
+
+
+if __name__ == '__main__':
+    c = Telas()
